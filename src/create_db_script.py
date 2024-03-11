16a15a475468678e8f4580fd50b8a755f1dd7635
Alochke <alonpolski@mail.tau.ac.il>
2024-03-11 19:47:26 +0200

    Finished create_db_script.py

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -1,112 +1,27 @@
 import os
 import pandas as pd
-from typing import Tuple
+import csv
 
-def create_base() -> Tuple[pd.DataFrame]:
+def create_genre() -> pd.DataFrame:
     """
-    This filters unneeded data. It's a refactoring function so we won't have to filter the data twice, when creating the title data frame and when
-    creating the episode data frame.
-    It's returned value should be passed to create_title and to create_episode.
-
-    Returns (Tuple[pd.DataFrame]):
-        A tuple containing two DataFrames - 'basics' and 'ratings'.
-
-    The 'basics' DataFrame contains information about titles, including a unique id, type (short, movie, video etc.), name, whether it is adult content,
-    the year at which the title production started and duration.
-    The 'ratings' DataFrame contains information about the average rating and the unique id of each title.
-
-    Note:
-    - The function assumes the existence of 'title.basics.csv' and 'title.ratings.csv' files in the 'data' directory.
-
-    Example:
-    basics_df, ratings_df = create_base()
-    """
-    # Reads the 'title.basics.csv' and 'title.ratings.csv' files located in the 'data' directory.
-    basics = pd.read_csv(os.path.join("data", "title.basics.csv") \
-                         , dtype={'tconst': str, 'titleType': str, 'primaryTitle': str, 'originalTitle': str, 'isAdult': str, 'startYear': str, 'endYear': str, 'runtimeMinutes': str, 'genres': str})
-    ratings = pd.read_csv(os.path.join("data", "title.ratings.csv") \
-                          , dtype={'tconst': str, 'averageRating': float, 'numVotes': int})
-
-    # Drops unnecessary data.
-    basics = basics[basics['isAdult'].isin(['0', '1'])] # We don't want any data ambivalent about being sexual sneaking into our data base. naughty, naughty!
-    basics.drop(inplace= True, columns= ["originalTitle", "endYear", "genres"])
-    ratings.drop(inplace = True, columns= ["numVotes"])
-
-    return basics, ratings
-
-def create_title(basics: pd.DataFrame, ratings: pd.DataFrame) -> pd.DataFrame:
-    """
-    Creates a data frame equivelent of the title table,
-    The only worth to mention diffrence is that the columns of the data frame have the name of the column they are based upon in the original datasets.
-
-    Parameters:
-        basics (pd.DataFrame): The first component of the tuple returned by create_base.
-        ratings (pd.DataFrame): The second component of the tuple returned by create_base.
-    
-    Returns:
-    A pd.DataFrame. Merged DataFrame containing relevant information from the two given data frames.
-
-    Example:
-    print(create_title(*create_base()))
-    """
-    temp = basics[~basics["titleType"].isin(['videoGame', 'tvPilot', 'tvSpecial', 'tvEpisode'])]
-
-    # Merges the processed DataFrames based on the 'tconst' column.
-    return pd.merge(temp, ratings, on = "tconst")
-
-def create_episode(basics: pd.DataFrame, ratings: pd.DataFrame) -> pd.DataFrame:
-    """
-    Creates a data frame equivelent of the episode table,
-    The only worth to mention diffrence is that the columns of the data frame have the name of the column they are based upon in the original datasets.
-
-    Parameters:
-        basics (pd.DataFrame): The first component of the tuple returned by create_base.
-        ratings (pd.DataFrame): The second component of the tuple returned by create_base.
-    
-    Returns:
-    A pd.DataFrame. Merged DataFrame containing relevant information from the two given data frames.
-
-    Example:
-    print(create_episode(*create_base()))
-    """
-    episode = pd.read_csv(os.path.join("data", "title.episode.csv") \
-                         , dtype={'tconst': str, 'parentTconst': str, 'seasonNumber': str, 'episodeNumber': str})
-
-    temp = pd.merge(basics, episode, on = "tconst")
-    return pd.merge(temp, ratings, on = "tconst")
-
-def create_genre(tconsts: pd.core.series.Series) -> pd.DataFrame:
-    """
-    Creating a data frame equivelent of the genre table,
-    The only worth to mention diffrence is that the columns of the data frame have the name of the column they are based upon in the original data set.
-
-    Parameters:
-        tconsts (pd.core.series.Series):
-            Should be the the union of the "tconst" columns of the data frames returned by create_episode and create_title,
-            will be used to drop rows that won't be used in the db,
-            becuase we don't store data about genres of titles we don't store data about in the title table or the episode table.
+    Creates a dataframe that represents the genres of movies.
     
     Returns:
     A pd.DataFrame. DataFrame containing relevant information from data/title.basics.csv.
+        The dataframe has two columns, id, a unique identifier of a title and genre, the genre of the title.
 
     Note:
     - This function assumes the existence of 'data' directory in the current working directory, containing the used csvs.
 
     Example:
-    episode = create_episode(*create_base())
-    title = create_title(*create_base())
-    create_genre(pd.concat([episode[["tconst"]], title[["tconst"]]])["tconst"])
+    genre = create_genre()
     """
-    genres_df = pd.read_csv(os.path.join("data", "title.basics.csv"),
-                            dtype={'tconst': str, 'titleType': str, 'primaryTitle': str, 'originalTitle': str, 'isAdult': str, 'startYear': str, 'endYear': str, 'runtimeMinutes': str, 'genres': str})
+    titles = pd.read_csv(os.path.join("data", "title.basics.csv"),
+                            dtype=  {'tconst': str, 'titleType': str, 'primaryTitle': str, 'originalTitle': str, 'isAdult': int, 'startYear': str, 'endYear': str, 'runtimeMinutes': str, 'genres': str},
+                            quoting= csv.QUOTE_NONE)
 
-    # Select only the relevant columns ('tconst' and 'genres')
-    genres_df = genres_df[['tconst', 'genres']]
 
-    # Filter rows based on the provided tconsts
-    genres_df = genres_df[genres_df['tconst'].isin(tconsts)]
-
-    # Initialize lists to store tconsts and genres
+    # Initialize lists to store ids and genres
     ids = []
     genres = []
 
@@ -117,80 +32,45 @@ def create_genre(tconsts: pd.core.series.Series) -> pd.DataFrame:
             genres.append(genre)
 
     # Apply the add_genre function to each row of the DataFrame
-    genres_df.apply(add_genre, axis=1)
+    titles.apply(add_genre, axis=1)
 
-    return pd.DataFrame({"tconst": ids, "genres": genres})
+    return pd.DataFrame({"id": ids, "genre": genres})
 
-def create_title_person(tconsts: pd.core.series.Series):
+def create_proffession() -> pd.DataFrame:
     """
-    Creating a data frame equivelent of the title_person table,
-    The only worth to mention diffrence is that the columns of the data frame have the name of the column they are based upon in the original data set.
-
-    Parameters:
-        tconsts (pd.core.series.Series):
-            Should be the the union of the "tconst" columns of the data frames returned by create_episode and create_title,
-            will be used to drop rows that won't be used in the db,
-            becuase we don't store data about titles we don't store data about in the title table or the episode table.
+    Creates a data frame equivalent of the proffession table.
     
     Returns:
-    A pd.DataFrame. DataFrame containing relevant information from data/title.principals.csv.
+    A pd.DataFrame. DataFrame containing relevant information from data/name.basics.csv,
+        The dataframe has two columns, id, a unique identifier of a person and profession, one of the professions of a person.
 
     Note:
     - This function assumes the existence of 'data' directory in the current working directory, containing the used csvs.
 
     Example:
-    episode = create_episode(create_base())
-    title = create_title(create_base())
-    create_title_person(pd.concat([episode[["tconst"]], title[["tconst"]]])["tconst"])
+    genre = create_genre()
     """
-    title_person = pd.read_csv(os.path.join("data", "title.principals.csv") \
-                         , dtype={'tconst': str, 'ordering': int, 'nconst': str, 'category': str, 'job': str, 'characters': str})
+    names = pd.read_csv(os.path.join("data", "name.basics.csv"),
+                        dtype={'nconst': str, 'primaryName': str, 'birthYear': str, 'deathYear': str, 'primaryProfession': str, 'knownForTitles': str}, 
+                        quoting = csv.QUOTE_NONE)
 
-    title_person = title_person[['tconst', 'nconst', 'category']]
-    title_person = title_person[title_person['tconst'].isin(tconsts)]
+    # Initialize lists to store ids and genres
+    ids = []
+    professions = []
 
-    return title_person
+    # Define a function to add genres for each row in the DataFrame
+    def add_profession(x: pd.core.series.Series):
+        # Some rows have the type of the 'primaryProfession' column as float which will result in an error, that's because the matching csv field
+        # is empty, so we drop their data, because it's not of any significance anyways.
+        if type(x['primaryProfession']) == str:
+                for genre in x['primaryProfession'].split():
+                    ids.append(x['nconst'])
+                    professions.append(genre)
 
-# def create_person(tconsts: pd.core.series.Series):
-#     """
-#     Creating a data frame equivelent of the title_person table,
-#     The only worth to mention diffrence is that the columns of the data frame have the name of the column they are based upon in the original data set.
+    # Apply the add_genre function to each row of the DataFrame
+    names.apply(add_profession, axis=1)
 
-#     Parameters:
-#         tconsts (pd.core.series.Series):
-#             Should be the the union of the "tconst" columns of the data frames returned by create_episode and create_title,
-#             will be used to drop rows that won't be used in the db,
-#             becuase we don't store data about titles we don't store data about in the title table or the episode table.
-    
-#     Returns:
-#     A pd.DataFrame. DataFrame containing relevant information from data/title.principals.csv.
+    return pd.DataFrame({"id": ids, "profession": professions})
 
-#     Note:
-#     - This function assumes the existence of 'data' directory in the current working directory, containing the used csvs.
-
-#     Example:
-#     episode = create_episode(create_base())
-#     title = create_title(create_base())
-#     create_title_person(pd.concat([episode[["tconst"]], title[["tconst"]]])["tconst"])
-#     """
-#     person = pd.read_csv(os.path.join("data", "name.basics.csv") \
-#                          , dtype={'nconst': str, 'primaryName': int, 'birthYear': int, 'deathYear': int, 'primaryProfession': str, 'knownForTitles': str})
-
-#     person = person[['nconst', 'nconst', 'category']]
-#     title_person = title_person[title_person['tconst'].isin(tconsts)]
-
-#return title_person
-
-if __name__ == "__main__":
-    base = create_base()
-    # title = create_title(*base)
-    # print(title)
-    # episode = create_episode(*base)
-    # print(episode)
-    # join = pd.concat([episode[["tconst"]],title[["tconst"]]])
-    # base = None
-    # episode = None
-    genre = create_genre(base[0]["tconst"])
-    print(genre)
-    title_person = create_title_person(base[0]["tconst"])
-    print(title_person)
+print(create_proffession())
+print(create_genre())
