723cd08d50aef0384a7828276843b2e794e285d6
Alochke <alonpolski@mail.tau.ac.il>
2024-03-13 13:01:05 +0200

    Updated create_db_script.py

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -1,7 +1,7 @@
 import mysql
 
-MOVIE_ID_LEN = 10
-PERSON_ID_LEN = 10
+TCONST_LEN = 10
+NCONST_LEN = 10
 MOVIE_LEN = 5
 MAX_MOVIE_NAME_LEN = 208
 MAX_MINUTES_LEN = 5
@@ -9,13 +9,15 @@ MAX_GENRE_LEN = 11
 MAX_JOB_LEN = 19
 MAX_PERSON_NAME_LEN = 95
 MAX_PROFESSION_LEN = 25
+TITLE_ID_LEN = 6
+PERSON_ID_LEN = 7
 
 def create_tables(cursor: mysql.connector.cursor_cext.CMySQLCursor):
     TABLES = {}
     TABLES['title'] = (
         "CREATE TABLE title("
-        "id MEDIUMINT(6) UNSIGNED NOT NULL AUTO_INCREMMENT UNIQUE"
-        f"temp CHAR({MOVIE_ID_LEN}) NOT NULL UNIQUE,"
+        f"id MEDIUMINT({TITLE_ID_LEN}) UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,"
+        f"temp CHAR({TCONST_LEN}) NOT NULL UNIQUE,"
         f"type CHAR({MOVIE_LEN}) NOT NULL,"
         f"name VARCHAR({MAX_MOVIE_NAME_LEN}) NOT NULL,"
         "adult BOOL NOT NULL,"
@@ -28,36 +30,38 @@ def create_tables(cursor: mysql.connector.cursor_cext.CMySQLCursor):
     )
     TABLES['genre'] = (
         "CREATE TABLE genre("
-        f""
-        f"temp char({MOVIE_ID_LEN}) NOT NULL,"
+        f"id MEDIUMINT({TITLE_ID_LEN}) UNSIGNED NOT NULL DEFAULT 0,"
+        f"temp char({TCONST_LEN}) NOT NULL,"
         f"genre varchar({MAX_GENRE_LEN}) NOT NULL,"
-        "PRIMARY KEY (id, genre),"
         "FOREIGN KEY (temp) REFERENCES title(temp)"
         ") ENGINE=InnoDB"
     )
     TABLES['title_person'] = (
         "CREATE TABLE title_person("
-        f"temp1 char({MOVIE_ID_LEN}) NOT NULL,"
-        f"temp2 char({PERSON_ID_LEN}) NOT NULL,"
+        f"film_id MEDIUMINT({TITLE_ID_LEN}) UNSIGNED NOT NULL DEFAULT 0,"
+        f"person_id MEDIUMINT({PERSON_ID_LEN}) UNSIGNED NOT NULL DEFAULT 0,"
+        f"temp1 char({TCONST_LEN}) NOT NULL,"
+        f"temp2 char({NCONST_LEN}) NOT NULL,"
         f"job varchar({MAX_JOB_LEN}) NOT NULL,"
-        "PRIMARY KEY (temp1, temp2, job),"
         "FOREIGN KEY (temp1) REFERENCES title(temp)"
         ") ENGINE=InnoDB"
     )
     TABLES['person'] = (
         "CREATE TABLE person("
-        f"temp char({PERSON_ID_LEN}) NOT NULL,"
+        f"person_id MEDIUMINT({PERSON_ID_LEN}) UNSIGNED NOT NULL DEFAULT 0,"
+        f"temp char({NCONST_LEN}) NOT NULL,"
         f"name varchar({MAX_PERSON_NAME_LEN}) NOT NULL,"
-        "FOREIGN KEY (temp) REFERENCES title_person(temp),"
-        "PRIMARY KEY (temp1)"
+        "FOREIGN KEY (temp) REFERENCES title_person(temp2)"
         ") ENGINE=InnoDB"
     )
+    print(TABLES['person'])
     TABLES['profession'] = (
         "CREATE TABLE profession("
-        f"id char({PERSON_ID_LEN}) NOT NULL,"
+        f"person_id MEDIUMINT({PERSON_ID_LEN}) UNSIGNED NOT NULL DEFAULT 0,"
+        f"temp char({NCONST_LEN}) NOT NULL,"
         f"profession varchar({MAX_PROFESSION_LEN}) NOT NULL,"
         "PRIMARY KEY (id, profession),"
-        "FOREIGN KEY (id) REFERENCES person(id)"
+        "FOREIGN KEY (temp) REFERENCES person(temp)"
         ") ENGINE=InnoDB"
     )
     
@@ -67,3 +71,4 @@ def create_tables(cursor: mysql.connector.cursor_cext.CMySQLCursor):
             cursor.execute(table_description)
         except mysql.connector.Error as err:
             print(err.msg)
+            print(table_name)
